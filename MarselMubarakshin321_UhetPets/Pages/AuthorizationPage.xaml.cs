5160c11310ba6515f95989e02f31b4cecc95f620
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2024-10-16 22:04:17 +0300

    Улучшена авторизация

--- MarselMubarakshin321_UhetPets/Pages/AuthorizationPage.xaml.cs
+++ MarselMubarakshin321_UhetPets/Pages/AuthorizationPage.xaml.cs
@@ -30,19 +30,62 @@ namespace MarselMubarakshin321_UhetPets.Pages
         }
         private void LoginBtn_Click(object sender, RoutedEventArgs e)
         {
+            // Получаем введенные данные
+            string username = LoginTextBox.Text;
+            string password = PasswordBox.Password;
+
+            // Проверяем пользователя в базе данных
             var user = DatabaseConnectionClass.DatabaseConnection.Users
-                .FirstOrDefault(u => u.Username == LoginTextBox.Text && u.Parol == PasswordBox.Password);
+                .FirstOrDefault(u => u.Username == username && u.Parol == password);
 
             if (user != null)
             {
                 if (user.Rolle == "Андрей пирокинезис")
+                {
                     NavigationService.Navigate(new PetsPage("Ра"));
+                }
                 else if (user.Rolle == "Деля")
+                {
                     NavigationService.Navigate(new PetsPage("Нуби"));
+                }
             }
             else
             {
                 MessageBox.Show("Неверный логин или пароль");
+            }
+        }
+
+        private void LoginTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (LoginTextBox.Text == "Введите логин")
+            {
+                LoginTextBox.Text = string.Empty;
+                LoginTextBox.Foreground = System.Windows.Media.Brushes.Black; 
+            }
+        }
+
+        private void LoginTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(LoginTextBox.Text))
+            {
+                LoginTextBox.Text = "Введите логин";
+                LoginTextBox.Foreground = System.Windows.Media.Brushes.Gray; // Меняем цвет текста на серый
+            }
+        }
+
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (PasswordBox.Password == string.Empty)
+            {
+                PasswordBox.Clear();
+            }
+        }
+
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(PasswordBox.Password))
+            {
+                
             }
         }
     }
